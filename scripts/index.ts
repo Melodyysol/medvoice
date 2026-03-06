c9b7e69f8cc3ea03a7821ffcee425dc4de2a4feb
Melodyysol <issaabdulwaris212@gmail.com>
2026-03-06 11:28:55 +0100

    Implement tsc and make it multiple pages with and some animations

---
+++ scripts/index.ts
@@ -0,0 +1,6 @@
+document.querySelector('.js-menu')?.addEventListener('click', () => {
+  const menu = document.querySelector('.js-menu + div');
+  if (menu) {
+    menu.classList.toggle('hidden');
+  }
+});
